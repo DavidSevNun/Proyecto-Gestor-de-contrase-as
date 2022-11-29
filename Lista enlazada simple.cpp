b2532461588b9dba2564732c7b709520a069d2d5
David Sevilla <abrilalejandro10@gmail.com>
2022-11-29 17:22:52 -0600

    Interfaz y gotoxy añadidos

--- Lista enlazada simple.cpp
+++ Lista enlazada simple.cpp
@@ -1,10 +1,30 @@
-//-------------prueba desde github
-//-------------Prueba1
 #include<iostream>
 #include<cstdlib>
-
+#include <conio.h>
+#include<windows.h>
+#include <string>
 using namespace std;
 
+void gotoxy(int x,int y){
+      COORD c;
+      c.X = x;
+      c.Y = y;
+      SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),c);
+
+ }
+//---------------GOTOXY
+
+void Interfaz(){
+    gotoxy(5,2); cout<<char(201)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(187);
+    gotoxy(5,3); cout<<char(186);
+    gotoxy(5,4); cout<<char(186); gotoxy(70,4); cout<<char(186); gotoxy(70,5); cout<<char(186);gotoxy(70,6); cout<<char(186); gotoxy(70,7); cout<<char(186);gotoxy(70,8); cout<<char(186); gotoxy(70,3); cout<<char(186);
+    gotoxy(5,5); cout<<char(186);
+    gotoxy(5,6); cout<<char(186);
+    gotoxy(5,7); cout<<char(186);
+    gotoxy(5,8); cout<<char(186);
+    gotoxy(5,9); cout<<char(200)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(188);
+    gotoxy(32,7);
+}//--------Interfaz
 
 
 class Nodo{
@@ -67,29 +87,35 @@ Nuevo->pswrd = contra;
         Nuevo->next = nullptr;
         tail = Nuevo;
     }
-
-    cout<<"Dato ingresado correctamente\n";
+    gotoxy(25,8); cout<<"Dato ingresado correctamente";
 };//-----------Insertar nodo
 
 
 void menu(){
+system("cls");
+Interfaz();
 int opc;
 string aux;
 lista list;
 
 do{
-cout<<"\n\n\nIngrese una opcion\n";
-cout<<"[1]Ingresar contrasena\t[2]Mostrar contrasenas\t[3]Salir\n\n";
-cout<<"Su opcion: ";
+    gotoxy(5,3); cout<<"\t\t           Ingrese una opcion";
+    gotoxy(5,5); cout<<char(186);cout<<"\t[1]Ingresar password\t[2]Mostrar passwords\t[3]Salir";
+    gotoxy(20,7); cout<<"Su opcion: ";
+
 cin>>opc;
 
     switch(opc){
 
     case 1:
     system("cls");
-        cout<<"Ingrese la contrasena: \n";
+        Interfaz();
+        gotoxy(20,3); cout<<"Ingrese su password: \n";
+        gotoxy(20,5); cout<<"Pswrd:________________________________";
+        gotoxy(32,5);
         cin>>aux;
         list.Insertar(aux);
+        system("pause");
     break;
 
     case 2:
@@ -97,8 +123,6 @@ cin>>opc;
         list.mostrar();
 
     break;
-
-
     }
 
 }while(opc!=3);
@@ -106,10 +130,36 @@ cin>>opc;
 }
 
 
+
 int main(){
-system("color 70");
+system("cls");
+system("color 07");
+//gotoxy(30,5); imprimir en el centro de interfaz
 
-menu();
+int opc;
+do{
+Interfaz();
+gotoxy(25,3); cout<<"[1]Log in\t[2]Sign in";
+gotoxy(30,6); cout<<"Opcion: [   ]"; gotoxy(40,6); cin>>opc;
 
+    switch(opc){
+
+    case 1:
+        menu();
+    break;
+
+    case 2:
+
+
+
+    break;
+
+    default:
+        gotoxy(30,5); cout<<"Opcion no valida";
+    break;
+
+    }
+
+}while(opc!= 1 && opc!=2);
 
 return 0;}
