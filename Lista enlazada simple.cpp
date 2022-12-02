599b6804cc2ec7576c18606c18df382d439ccc5e
David Sevilla <abrilalejandro10@gmail.com>
2022-12-01 21:04:56 -0600

    Agregué algunas validaciones, y otros detalles esteticos.  sign in y log in ya funcionan correctamente

--- Lista enlazada simple.cpp
+++ Lista enlazada simple.cpp
@@ -6,6 +6,23 @@
 #include <fstream>
 using namespace std;
 
+ bool validarNum(char op[]){
+                int i;
+                for (i=0; i<strlen(op); i++){
+
+                    if((!(op[i]>=48 && op[i]<=57)&&op[i]!=46)){
+                        cout<<endl;
+                        cout<<"\tSOLO INGRESE NUMEROS"<<endl<<endl;
+                        system("pause>>cls");
+                         return false;
+
+                    }
+                    }
+                return true;
+                }
+//----------------VALIDAR NUMEROS
+
+
 void gotoxy(int x,int y){
       COORD c;
       c.X = x;
@@ -16,14 +33,14 @@ void gotoxy(int x,int y){
 //---------------GOTOXY
 
 void Interfaz(){
-    gotoxy(5,2); cout<<char(201)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(187);
+    gotoxy(5,2); cout<<char(201)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(187);
     gotoxy(5,3); cout<<char(186);
-    gotoxy(5,4); cout<<char(186); gotoxy(70,4); cout<<char(186); gotoxy(70,5); cout<<char(186);gotoxy(70,6); cout<<char(186); gotoxy(70,7); cout<<char(186);gotoxy(70,8); cout<<char(186); gotoxy(70,3); cout<<char(186);
+    gotoxy(5,4); cout<<char(186); gotoxy(78,4); cout<<char(186); gotoxy(78,5); cout<<char(186);gotoxy(78,6); cout<<char(186); gotoxy(78,7); cout<<char(186);gotoxy(78,8); cout<<char(186); gotoxy(78,3); cout<<char(186);
     gotoxy(5,5); cout<<char(186);
     gotoxy(5,6); cout<<char(186);
     gotoxy(5,7); cout<<char(186);
     gotoxy(5,8); cout<<char(186);
-    gotoxy(5,9); cout<<char(200)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(188);
+    gotoxy(5,9); cout<<char(200)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(205)<<char(188);
     gotoxy(32,7);
 }//--------Interfaz
 
@@ -46,71 +63,191 @@ class lista{
 
 public:
     void RegistrarUsuario();
-    void Insertar(string);
+    void Insertar(string,string);
     void modify();
-    void LogIn(string);
+    void LogIn(string,string);
 
     void mostrar();
 
 };//--------CLASS LISTA
 
+
+// Encriptado y decriptado XOR
+string XOR(string dato){
+	char key = 'K';
+	string output = dato;
+	
+	for (int i= 0; i < dato.size(); i++)
+	 output[i] = dato[i] ^ key;
+	 
+	return output;
+}//------------------------------
+
+
 void menu(){
-system("cls");
-Interfaz();
 int opc;
-string aux;
+string aux,aux2,data;
 lista list;
 
 do{
+    system("cls");
+    Interfaz();        
     gotoxy(5,3); cout<<"\t\t           Ingrese una opcion";
-    gotoxy(5,5); cout<<char(186);cout<<"\t[1]Ingresar password\t[2]Mostrar passwords\t[3]Salir";
+    gotoxy(5,5); cout<<char(186);cout<<"[1]Ingresar password\t[2]Mostrar sitios\t[3]Modificar password";
+    gotoxy(65,8); cout<<"[4]Salir";
     gotoxy(20,7); cout<<"Su opcion: ";
     cin>>opc;
 
     switch(opc){
 
-    case 1:
-    system("cls");
-        Interfaz();
-        gotoxy(20,3); cout<<"Ingrese su password: \n"; //pedirle al usuario el sitio
-        gotoxy(20,5); cout<<"Pswrd:________________________________";
-        gotoxy(32,5);
-        cin>>aux;
-        //encriptar, return pswrd encriptada y mandarla a insertar();
-        list.Insertar(aux);
-        system("pause");
-    break;
+        case 1:
+            system("cls");
+            Interfaz();
+            gotoxy(20,3); cout<<"Ingrese el sitio:"; //pedirle al usuario el sitio
+            gotoxy(20,5); cout<<"sitio: ";
+            gotoxy(27,5);
+            cin>>aux;
+            for (int i=0;i<aux.length();i++){
 
-    case 2:
-    
-        system("cls");
-        list.mostrar();
+                aux[i] = toupper(aux[i]);//Convertir string a mayusculas
 
-    break;
+            }
+
+            system("cls");
+            Interfaz();
+            gotoxy(20,4); cout<<"Ingrese su password: ";
+            gotoxy(20,6); cout<<"password: ";
+            gotoxy(32,6);
+            cin>>aux2;
+            aux2 = XOR(aux2);
+        
+            list.Insertar(aux,aux2);
+            system("pause");
+
+        break;
+
+        case 2:
+        
+            system("cls");
+            list.mostrar();
+
+        break;
+
+
+        case 3:
+            
+            list.modify();
+
+        break;
+
+
+
     }
 
-}while(opc!=3);
+}while(opc!=4);
 
 }
 
+void lista::modify(){
+    system("cls");
+    string aux,linea;
+    ifstream file;
+    
+
+        if(head==nullptr){
+
+            system("cls");
+            Interfaz();
+            gotoxy(25,6);    cout<<"Lista vacia";
+
+        }else{
+
+            Interfaz();
+            gotoxy(5,3); cout<<"Ingrese el sitio: ";
+            cin>>aux;
+
+            for (int i=0;i<aux.length();i++){
+
+                aux[i] = toupper(aux[i]);//Convertir string a mayusculas
+
+            }
+
+            if(file.is_open()){
+
+                 while(!file.eof()){
+
+                    while(getline(file,linea)){
+
+                        if(linea == aux){ //buscar coincidencias dentro del txt. 
+
+                            Nodo *ptr = head;
+                              do{
+                                if(ptr->sitio == aux){
+
+                                    string pass;
+                                    system("cls");
+                                    Interfaz();
+                                    gotoxy(25,6);
+                                    cout<<"Ingrese la nueva password:\n";
+                                    cin>>pass;
+                                    ptr->pswrd = pass;
+
+                                    }  
+
+                                    ptr = ptr->next;
+                                }while(ptr!=nullptr);
+
+                            }else{
+                                system("cls");
+                                Interfaz();
+                                gotoxy(25,6);    cout<<"No se encontro el sitio";
+
+                            }
+                        }
+                    }
+                file.close();    
+                }//IF file open
+        }//IF vacia
+
+    
+}
+
+
 void lista::mostrar(){
+    system("cls");
+    Interfaz(); 
+    gotoxy(15,2); cout<<"Sitios donde tiene passwords guardadas: ";
 Nodo *list;
 int i=1;
-
+int cont = 0;
 list = head;
 
     if(head!=nullptr){
 
         do{
+                if(cont==4){
 
-            cout<<i<<".-"<<list->sitio<<endl;
+                    gotoxy(10,3); cout<<i<<".-"<<list->sitio<<endl;
+                    cont = 0;
+
+                }else{
+
+                    cout<<i<<".-"<<list->sitio<<"  ";
+                    cont++;
+                }
+            
             list = list->next;
             i++;
         }while(list!=nullptr);
 
         system("pause");
 
-    }cout<<"Lista vacia\n";
+    }else{
+        system("cls");
+        Interfaz();
+        gotoxy(30,6);        cout<<"Lista vacia\n";
+
+    }
 }//-------------MOSTRAR LISTA------------
 
 void lista::RegistrarUsuario(){
@@ -118,27 +255,33 @@ system("cls");
 Interfaz();
 ofstream file;
 string user,pswrd,txt;
-file.open("contrasenas.txt",fstream::out |fstream::app);
+file.open("LogIn.txt",fstream::out |fstream::app);
 
     if(file.is_open()){
 
         gotoxy(25,4); cout<<"Ingrese el nombre de usuario:";
         cin>>user;
+        for (int i=0;i<user.length();i++){
+
+            user[i] = toupper(user[i]);
+
+         }
+
         gotoxy(25,6); cout<<"Ingrese su password:";
         cin>>pswrd;
-        txt = user + " " + pswrd; //Ya creada la funcion de encriptado, mandarle desde aqui los datos y guardarlos en el txt
-        //file<<txt<<endl;
+        pswrd = XOR(pswrd);
+        txt = user + " " + pswrd; 
+        file<<txt<<endl;
         file.close();
     }
-    menu();
+    
 }
 
-void lista::LogIn(string datos){
-system("cls");
-Interfaz();
+void lista::LogIn(string datos,string user){
 ifstream file; 
 string linea;
-file.open("contrasenas.txt",fstream::out |fstream::app);
+system("pause");
+file.open("LogIn.txt",ios::in);
 
     if(file.is_open()){
 
@@ -147,26 +290,40 @@ file.open("contrasenas.txt",fstream::out |fstream::app);
             while(getline(file,linea)){
 
                 if(linea == datos){ //buscar dentro del txt.  datos es el usuario y contraseña ingresados
-
-                    gotoxy(25,4); cout<<"Sesion iniciada";
+                    
+                    system("cls");
+                    Interfaz();
+                    gotoxy(30,4); cout<<"Sesion iniciada";
+                    gotoxy(30,6); cout<<"Bienvenido, "<<user<<endl;
+                    gotoxy(25,8); system("pause");
                     menu();
 
                 }else{
-
-                    cout<<"No se encontro";
+                    system("cls");
+                    Interfaz();
+                    cout<<"No se encontro el usuario\n";
+                    cout<<"\t";system("pause");
 
                 }
             }
         }
        file.close();    
+    }else{
+        system("cls");
+        Interfaz();
+        gotoxy(15,6); cout<<"No hay usuarios registrados...\n";
+        gotoxy(20,8); system("pause");
     }
 }
 
 
-void lista::Insertar(string contra){
-
+void lista::Insertar(string sitio,string pass){
+string data;
 Nodo *Nuevo = new Nodo();
-Nuevo->pswrd = contra;
+ofstream file;
+
+Nuevo->sitio = sitio;
+Nuevo->pswrd = pass;
 
     if(head==nullptr)
     {
@@ -181,34 +338,63 @@ Nuevo->pswrd = contra;
         tail = Nuevo;
 
     }
-    gotoxy(25,8); cout<<"Dato ingresado correctamente";
-};//-----------Insertar nodo
+        data = sitio + " " + pass;
+            
+            file.open("contrasenas.txt",ios::out|ios::app);
+
+            if(file.is_open()){
+
+                file<<data<<endl;//guardar datos en txt
+
+            }
+    gotoxy(25,8); cout<<"Dato ingresado correctamente\n";
+};//----------------------------------------------------Insertar nodo
 
 
 
-
-
+//-----------------------------MAIN---------------------------
 int main(){
-system("cls");
+
 system("color 07");
 //gotoxy(30,5); imprimir en el centro de interfaz
 lista list;
 int opc;
-string user, pswrd,txt;
+char OPC[5];
+bool val = false;
+string user, pswrd,txt,aux;
+
 do{
-Interfaz();
-gotoxy(25,3); cout<<"[1]Log in\t[2]Sign in";
-gotoxy(30,6); cout<<"Opcion: [   ]"; gotoxy(40,6); cin>>opc;
+     do{
+        system("cls");
+        Interfaz();
+        gotoxy(30,3); cout<<"[1]Log in\t[2]Sign in"; 
+        gotoxy(65,8); cout<<"[3]Salir";
+        gotoxy(35,6); cout<<"Opcion: [   ]"; gotoxy(45,6); cin>>OPC;
+        
+        val=validarNum(OPC);
+
+    }while(val==false);
+
+        opc=atoi(OPC);
 
     switch(opc){
 
     case 1:
-        gotoxy(25,3); cout<<"Ingrese su usuario: ";
+        system("cls");
+        Interfaz();
+        gotoxy(20,3); cout<<"Ingrese su usuario: ";
         cin>>user;
-        gotoxy(25,5); cout<<"Ingrese su password: ";
+        for (int i=0;i<user.length();i++){
+
+            user[i] = toupper(user[i]);//Convertir string a mayusculas
+
+         }
+
+        gotoxy(20,5); cout<<"Ingrese su password: ";
         cin>>pswrd;
-        txt = user + " " + pswrd;
-        list.LogIn(txt);
+        aux = XOR(pswrd);
+        txt = user + " " + aux;
+        list.LogIn(txt,user);
 
     break;
 
@@ -219,11 +405,12 @@ gotoxy(30,6); cout<<"Opcion: [   ]"; gotoxy(40,6); cin>>opc;
     break;
 
     default:
-        gotoxy(30,5); cout<<"Opcion no valida";
+            system("cls");
     break;
 
     }
 
-}while(opc!= 1 && opc!=2);
+}while(opc!=3);
+
 
 return 0;}
